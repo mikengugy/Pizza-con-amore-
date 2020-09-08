dca1f11de3e943ee6b1d490da638657855c6cd2f
mikengugy <mikengugy@gmail.com>
2020-09-08 21:17:42 +0300

    modified js function

--- js/script.js
+++ js/script.js
@@ -73,3 +73,53 @@ Order.prototype.pizzaCost = function(){
 
 
 // User interface logic
+$(document).ready(function(){
+    $("form#custom-pizza").submit(function(event){
+        event.preventDefault();
+        var customSize = $("select#size").val();
+        var crust = $("select#crust").val();
+        var pepperoni = $("input#pepperoni").val();
+        var cheese = $("input#cheese").val();
+        var bacon = $("input#bacon").val();
+        var mushrooms = $("input#mushrooms").val();
+        var onions = $("input#onions").val();
+        var sausage = $("input#sausage").val();
+        var spinach = $("input#spinach").val();
+        var blackOlives = $("input#blackOlives").val();
+        var quantity = parseInt($("select#qty").val());
+        var delivery = $("select#sel1").val(); 
+        var val = [];
+        $(':checkbox:checked').each(function(i){
+          val[i] = $(this).val();
+        });
+        var newPizzaOrder = new Order(customSize, crust);
+        newPizzaOrder.toppings.push(val);
+        var cost = (parseInt(newPizzaOrder.pizzaCost()) * quantity);
+        var deliveryCost = (parseInt(newPizzaOrder.pizzaCost()) * quantity) + 300;
+        //checkout 
+        if (delivery === "Self Pick"){
+            $("#final-cost").show();
+            $("#final-cost").text(cost);
+        }
+        else{
+            $("#final-cost").show();
+            $("#final-cost").text(deliveryCost);
+            var location = prompt("Please enter your location");
+            alert("You order has been received and will be delivered to your location in " + location + ". " + " Please proceed to checkout to see more details.")
+            alert("You will be charged a delivery cost of Ksh. 300 and your total cost will be " + deliveryCost );
+        }
+        
+        
+    });
+
+    $("#final-cost").click(function() {
+      });
+
+    $("#checkout-btn").click(function() {
+        location.reload();
+      });
+    $("form#write").submit(function(){
+        var name = $("#write").val();
+        alert("We have received your message. Thank you for reaching out to us.")
+    });
+});
